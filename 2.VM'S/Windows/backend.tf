c6fc481f709311647ad9e08ab86f86b2aa85a640
sathvik2k <sathvik2k@outlook.com>
2022-09-20 05:06:01 +0000

    Updated backend.tf

--- 2.VM'S/Windows/backend.tf
+++ 2.VM'S/Windows/backend.tf
@@ -1,8 +1,8 @@
 # terraform backend configuration for terraform.tfstate file
 terraform{
   backend "azurerm"{
-     # storage_account_name = "terraform2112"
-     # container_name       = "terraform5212"
+     # storage_account_name = "tfstg-001"
+     # container_name       = "tfcnt-001"
      # key                  = "terraform.tfstate"
      # use_azuread_auth     = "true"
      # subscription_id      = "0c895a63-6145-4be8-aa9a-7fe3b83064ae"
